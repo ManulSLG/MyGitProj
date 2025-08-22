302164833db2f19fdf7e11521dc98ba5a7f54089
ManulSLG <panukov.de6@gmail.com>
2025-08-22 16:42:59 +0400

    test1

---
+++ UnityLearning/learninconsole/learninconsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learninconsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            string dayOfWeek;
+
+            Console.Write("Введите день недели: ");
+            dayOfWeek = Console.ReadLine();
+
+            switch(dayOfWeek)
+            {
+                case "понедельник":
+                    Console.WriteLine("лол");
+                    break;
+                case "вторник":
+                    Console.WriteLine("кек");
+                    break;
+                case "среда":
+                    Console.WriteLine("чебурек");
+                    break;
+                case "четверг":
+                    Console.WriteLine("умный человек в очках");
+                    break;
+                case "пятница":
+                    Console.WriteLine("понедельник");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
